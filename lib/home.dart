34684ad4d6deb446acbe2ec26bb0c98f5a763c3a
pedrocattel <pedrocattel@hotmail.com>
2020-06-26 18:56:18 -0300

    links arrumados

--- lib/home.dart
+++ lib/home.dart
@@ -43,13 +43,24 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(12.0),
           child: ListView(
             children: <Widget>[
-              Card(child: ListTile(title: Text(''))),
-              Card(child: ListTile(title: Text('Governo do Brasil'))),
-              Card(child: ListTile(title: Text('Estatísticas Globais'))),
+              Container(
+                padding:EdgeInsets.only(left: 20),
+                child: Text("Fontes verificadas",style: GoogleFonts.lato(fontSize: 24, color: Colors.white),textAlign: TextAlign.start,),
+              ),
+              Divider(
+                color: Colors.white,
+                height: 15,
+                thickness: 2,
+                indent: 17,
+                endIndent: 30,
+              ),
+              Card(child: ListTile(title: Text('Estatísticas Globais',style: GoogleFonts.lato()),onTap: _launchURLstat)),
+              Card(child: ListTile(title: Text('Governo do Brasil',style: GoogleFonts.lato()),onTap: _launchURLgoverno)),
+              Card(child: ListTile(title: Text('Ministério da Saúde',style: GoogleFonts.lato()),onTap: _launchURLministerio)),
+              Card(child: ListTile(title: Text('Nações Unidas',style: GoogleFonts.lato()),onTap: _launchURLnacoes)),
             ],
           ),
         )
-
     );
   }
 
@@ -66,6 +77,7 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.only(top: 20.0),
           children: <Widget>[
             ListTile(
+              trailing: Icon(Icons.assignment_turned_in,color: Colors.white),
               title: Text(
                 'Corona Quiz',
                   style: GoogleFonts.lato(fontSize: 16,color: Colors.white),
@@ -86,7 +98,7 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               title: Text(
-                'Aplicativo desenvolvido com o intuito de ajudar a população durante a pandêmia, em caso grave procure as autoridades!',
+                'Aplicativo desenvolvido com o intuito de ajudar a população durante a pandêmia, em caso de sintomas graves procure as autoridades!',
                 style: GoogleFonts.lato(fontSize: 16,color: Colors.white),
               ),
             ),
@@ -96,12 +108,40 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _launchURL() async {
-    const url = 'https://uol.com.br';
+  _launchURLgoverno() async {
+    const url = 'https://covid.saude.gov.br/';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw 'Could not launch $url';
     }
   }
+
+  _launchURLstat() async {
+    const url = 'https://corona.help/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _launchURLnacoes() async {
+    const url = 'https://nacoesunidas.org/tema/coronavirus/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _launchURLministerio() async {
+    const url = '  https://coronavirus.saude.gov.br/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 }
